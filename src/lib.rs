1a95e391ae605f376c865a47dd0149603430b75f
Nick Fitzgerald <fitzgen@gmail.com>
2016-07-22 17:13:35 -0700

    Add initial code generated by bindgen

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+//! TODO
+
+extern crate libc;
+
+pub mod arch;
+// pub mod arm64;
+// pub mod compact_unwind_encoding;
+// pub mod dyld;
+// pub mod dyld_images;
+// pub mod fat;
+// pub mod getsect;
+// pub mod i386;
+// pub mod ldsyms;
+// pub mod lib;
+// pub mod loader;
+// pub mod nlist;
+// pub mod ranlib;
+// pub mod reloc;
+// pub mod stab;
+// pub mod swap;
+// pub mod x86_64;
